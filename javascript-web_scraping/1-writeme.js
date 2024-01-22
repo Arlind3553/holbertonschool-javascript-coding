769cc338888876b160f0ef2b3a9195595bf7e376
arlind3553 <georgetesco@yahoo.com>
2024-01-22 11:59:02 +0000

    task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const arg2 = process.argv[2];
 const arg3 = process.argv[3];
-fs.writeFile(arg2, arg3, content, err => {
+fs.writeFile(arg2, arg3, err => {
   if (err){
     console.log(err);
   }
