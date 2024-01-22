c710b872c18719f258171117fa1fe504bf320942
arlind3553 <georgetesco@yahoo.com>
2024-01-22 13:12:31 +0000

    task 1

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const process = require('process');
-const l = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2]
 
-request(l, (error, response, body) => {
+const link = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(link, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else if (response.statusCode != 200) {
-    console.error(`Response code : ${response.statusCode}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`Response code: ${response.statusCode}`);
   } else {
-    console.log(JSON.parse(body).tittle);
+    const results = JSON.parse(body);
+    console.log(results.title);
   }
 });
