4c80284fff6db86665a0b227b3877cec7fd07e6e
Arlind3553 <135588828+Arlind3553@users.noreply.github.com>
2024-01-23 14:06:32 +0100

    Update 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,9 +5,9 @@ const api = process.argv[2];
 
 request(api, function (error, response, body) {
   if (error) {
-    console.error('error:', error); // Print the error if one occurred
+    console.error('error:', error);
   }
-  const films = JSON.parse(response.body); // Print the response status code if a response was received
+  const films = JSON.parse(response.body); 
   const filtered = films.results.filter(film => {
     for (const character of film.characters) {
       if (character.includes('18')) {
