c140770a0404e0ee9eb8f6cbf1e2018a0f20760d
arlind3553 <georgetesco@yahoo.com>
2024-01-31 13:43:55 +0000

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ const readline = require('readline').createInterface({
     input: process.stdin,
 });
 readline.question('', name => {
-    process.stdout.write('Your name is:' + name + '\r');
+    process.stdout.write('Your name is: ' + name + '\r');
     console.log('This important software is now closing');
     readline.close();
 });
