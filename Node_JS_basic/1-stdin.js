2b9f2141c93de99bdffcd4ba00832d8f8b99e8ce
arlind3553 <georgetesco@yahoo.com>
2024-01-31 13:22:19 +0000

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+console.log('Welcome to Holberton School, what is your name?');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+});
+readline.question('', name => {
+    console.log(`Your name is: ${name}!`);
+    console.log('This important software is now closing');
+    readline.close();
+});
